c9a9eecdda8ca66066376af460c62567ad0fbc76
Muhammad Roman <mromankhan005@gmail.com>
2025-05-22 23:15:32 +0500

    AI Sales Agent Server

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000/", "https://sales-agent-eta.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
